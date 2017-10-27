a5f577cdf3ae408e91fb2f555ffa9419b568255c
darleneagbayani <31275691+darleneagbayani@users.noreply.github.com>
2017-10-26 19:00:30 -1000

    commit

--- app/imports/ui/pages/edit-contact-page.js
+++ app/imports/ui/pages/edit-contact-page.js
@@ -56,5 +56,10 @@ Template.Edit_Contact_Page.events({
       instance.messageFlags.set(displayErrorMessages, true);
     }
   },
+  'click .delete'(event, instance) {
+    event.preventDefault();
+    Contacts.remove(FlowRouter.getParam('_id'));
+    FlowRouter.go('Home_Page');
+  }
 });
 
